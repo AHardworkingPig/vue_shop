56694cc666c727d93d9dbdb475f3b25995f3bd4a
hjl <3330450435@qq.com>
2020-11-01 14:54:05 +0800

    dev user Completion

--- src/plugins/element-ui.ts
+++ src/plugins/element-ui.ts
@@ -4,7 +4,7 @@ import {
     Header, Aside, Main, Submenu, Menu, MenuItem,
     Breadcrumb, BreadcrumbItem, Card, Col, Row,
     Table, TableColumn, Switch, Tooltip, Pagination,
-    Dialog, MessageBox,
+    Dialog,
 } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -32,4 +32,3 @@ Vue.use(Pagination);
 Vue.use(Dialog);
 
 Vue.prototype.$message = Message;
-Vue.prototype.$confirm = MessageBox;
